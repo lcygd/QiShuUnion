e1db74ad65c2b17635023a3ff5262f5f195b563e
Lcy <1012104420@qq.com>
2025-05-25 19:41:43 +0800

    【流程】界面、开关按钮

---
+++ Assets/CS/SwitchBtn.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwitchBtn : MonoBehaviour
+{
+    [SerializeField] Image onImage;
+
+    [SerializeField] private bool isOn;
+
+    public void OnClick()
+    {
+        isOn = !isOn;
+        UpdateShow();
+    }
+
+    public void UpdateShow()
+    {
+        onImage.gameObject.SetActive(isOn);
+    }
+
+    // 在编辑器模式下修改值时调用UpdateShow
+    private void OnValidate()
+    {
+        UpdateShow();
+    }
+
+
+}
